c31ba4baa72defc7579ecb5572384a0cd7bd01f8
Imggaggu <ggaggu@snu.ac.kr>
2025-01-02 01:02:10 +0900

    JavaScript - displayitems

--- Javascript 쇼핑몰 미니게임 과제/main.js
+++ Javascript 쇼핑몰 미니게임 과제/main.js
@@ -7,11 +7,29 @@ function loadItems(){
 }
 
 
+//update list with givne item
+function displayItems(items) {
+    const container= document.querySelector('.items');
+    const html =items.map(item => createHTMLString(item));
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
+
+//create html list from the given data
+function createHTMLString(item){
+    return `
+    <li class="item">
+        <img src="${item.image}" alt="${item.type}" class="item__thumbnail"/>
+        <span class="item__description"> ${item.gender}, ${item.size}</span>
+    </li>
+    `;
+}
+
+
 //main
 loadItems()
 .then(items => {
     console.log(items);
- //   displayItems(items);
+    displayItems(items);
  //   setEventListeners(items)
 })
 .catch(console.log);
