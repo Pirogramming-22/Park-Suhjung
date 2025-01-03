f1e31671a2cf9ef4e10104e9ba308f3a19858b9a
Imggaggu <ggaggu@snu.ac.kr>
2025-01-04 02:50:21 +0900

    3단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,2 +1,18 @@
 num =0
-int (input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :"))
+def check_num():
+    while True:
+        num_of_nums=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+        if not num_of_nums.isdigit():
+            print("정수를 입력하세요")
+            continue
+   
+
+        num_of_nums = int(num_of_nums)
+        
+        if num_of_nums not in (1,2,3):
+            print("1, 2, 3 중 하나를 입력하세요")
+            continue
+
+
+        break
+check_num()
