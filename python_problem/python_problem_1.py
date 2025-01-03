151f45143f854102d4e2a4bb7915778666752329
Imggaggu <ggaggu@snu.ac.kr>
2025-01-04 03:21:22 +0900

    9단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,4 +1,5 @@
 num =0
+from random import randint
 def check_num():
     while True:
         num_of_nums=input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
@@ -17,23 +18,30 @@ def check_num():
         break
     return(num_of_nums)
 
-def brGame(player):
-    global num
-    nums = check_num()
 
-    for i in range(num, num + nums):
-        print(f"{player} : {i + 1}")
-        if i + 1 == 31:  
-            print(f"{player} wins!")
-            return True  
-    num += nums  
-    return False
+def player():
+    global num 
+    nums=check_num()
 
+    for i in range (num,num+nums):
+        print(f"player : {i+1}")
+        if i + 1 == 31: 
+            print("player wins!")
+    num=num+nums
 
 
-while num < 31:
-    if brGame("playerA"):  
-        break
-    if num < 31:  
-        if brGame("playerB"):  
-            break
+def computer():
+    global num 
+    nums=randint(1,3)
+
+    for i in range (num,num+nums):
+        print(f"computer : {i+1}")
+        if i + 1 == 31: 
+            print("computer wins!")
+    num=num+nums
+
+while num<31:
+    computer()
+    if num<31:
+        
+        player()
